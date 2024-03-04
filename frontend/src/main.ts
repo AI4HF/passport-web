8e0a173da4c11392a9c6c60ca2654b471488e56d
keremyilmaz <keremyilmaz499@gmail.com>
2024-03-04 11:11:46 +0000

    :sparkles: General styles which uses the application's pages at its base. Sets up the web page icon and name.

---
+++ frontend/src/main.ts
@@ -0,0 +1,10 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+//Main method which simply calls for the main app with the settings set in index.html
+//index.html also sets up the AI4HF icon in the website.
