33baeb3279fb816045a74b717ecf1c5934daa03d
keremyilmaz <keremyilmaz499@gmail.com>
2024-03-08 14:27:24 +0000

    :art: Angular Forms implemented into the structure

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,8 @@
 import {Component} from '@angular/core';
 import {ApiService} from './../../service/api.service';
 import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
-/**
- * Login component script which works along with the API Service and the Router identified above
- */
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,14 +10,13 @@ import {Router} from "@angular/router";
 })
 
 /**
- * Login message which changes depending on the result of the authentication attempt
+ * Login component script which works along with the API Service and the Router identified above
  */
 export class LoginComponent {
   /**
-   * Login form elements that are passed to the login request
+   * Login form which gets its elements from the variables in the html and passes them to the login method
    */
-  username: string = '';
-  password: string = '';
+  loginForm: FormGroup;
 
   /**
    * Login message that is set based on the login request result
@@ -32,7 +29,20 @@ export class LoginComponent {
   isLoginSuccess: boolean = false;
   isLoginError: boolean = false;
 
-  constructor(private apiService: ApiService, private router: Router) {
+  /**
+   * @param apiService Service which provides the methods used in this script
+   * @param router Router which will handle the routes that will be taken from this page
+   * @param formBuilder Utility which builds the login form from the data from the html variables
+   */
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
   /**
@@ -40,18 +50,21 @@ export class LoginComponent {
    * Redirections will be available after the implementation of the main page
    */
   login() {
-    this.apiService.login(this.username, this.password).subscribe(
-      (response) => {
-        this.loginMessage = 'Login successful!';
-        this.isLoginSuccess = true;
-        this.isLoginError = false;
-      },
-      (error) => {
-        this.loginMessage = 'Login failed. Please check your credentials.';
-        this.isLoginSuccess = false;
-        this.isLoginError = true;
-        console.error('Login failed:');
-      }
-    );
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      this.apiService.login(username, password).subscribe(
+        (response) => {
+          this.loginMessage = 'Login successful!';
+          this.isLoginSuccess = true;
+          this.isLoginError = false;
+        },
+        (error) => {
+          this.loginMessage = 'Login failed. Please check your credentials.';
+          this.isLoginSuccess = false;
+          this.isLoginError = true;
+          console.error('Login failed:', error);
+        }
+      );
+    }
   }
 }
