c79c47e37edee295a78632c60a522389ff4cb3d6
keremyilmaz <keremyilmaz499@gmail.com>
2024-03-08 13:48:32 +0000

    :sparkles: API URL has been changed to be provided in a separate environment file. Proper documentation is provided, and the flag which allows .ts files to be imported is set to true in the tsconfig file

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {catchError, map, Observable, of} from 'rxjs';
+import { environment } from '../environments/environment.ts';
 
 @Injectable({
   providedIn: 'root',
@@ -10,19 +11,26 @@ import {catchError, map, Observable, of} from 'rxjs';
  * API Service which hosts the available HTTP Requests to the scripts as functions
  */
 export class ApiService {
-  private apiUrl = 'http://localhost:8080/';
-
-  constructor(private http: HttpClient) {
-  }
-
+  /**
+   * @private apiUrl which the request are being sent to
+   */
+  private readonly apiUrl: string;
 
   /**
+   * @param http
+   * apiUrl is set to the url provided in the environment.ts file
+   */
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl;
+  }
+
+  /**
+   * @param username Username that will be provided in the form
+   * @param password Password that will be provided in the form
    * Login request which returns a List of Strings, which is made of the roles of the logged-in user
    */
   login(username: string, password: string): Observable<any> {
     const url = `${this.apiUrl}user/login`;
     return this.http.post(url, {username, password});
   }
-
-
 }
