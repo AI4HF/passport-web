90a201566565d70d38b104b81ca47d551cf6c53d
keremyilmaz <keremyilmaz499@gmail.com>
2024-03-12 13:26:04 +0000

    :bug: Another issue related to the component's allowing .ts based imports has been partially solved by reverting the change. A new method on using the environment file for request URL will be implemented shortly.

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {catchError, map, Observable, of} from 'rxjs';
-import { environment } from '../environments/environment.ts';
-
-@Injectable({
-  providedIn: 'root',
-})
 
 /**
  * API Service which hosts the available HTTP Requests to the scripts as functions
  */
+@Injectable({
+  providedIn: 'root',
+})
+
 export class ApiService {
   /**
    * @private apiUrl which the request are being sent to
@@ -17,17 +16,17 @@ export class ApiService {
   private readonly apiUrl: string;
 
   /**
-   * @param http
    * apiUrl is set to the url provided in the environment.ts file
+   * @param http
    */
   constructor(private http: HttpClient) {
-    this.apiUrl = environment.apiUrl;
+    this.apiUrl = "http://localhost:8080/";
   }
 
   /**
+   * Login request which returns a Token String, which can be used for further authentication and to obtain
    * @param username Username that will be provided in the form
    * @param password Password that will be provided in the form
-   * Login request which returns a List of Strings, which is made of the roles of the logged-in user
    */
   login(username: string, password: string): Observable<any> {
     const url = `${this.apiUrl}user/login`;
