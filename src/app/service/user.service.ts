2fe508a5b52acb345279adc974c1b9108b8ab7d1
keremyilmaz <keremyilmaz499@gmail.com>
2024-03-26 10:46:19 +0000

    :memo: User Service documentation has been created.

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+/**
+ * Class which holds role data instances after they're read from token resources.
+ */
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * New service class which helps calling users' roles to be stored.
+ */
 export class UserService {
+  /**
+   * Variable which is directly used to store and get the roles of the desired user.
+   */
   private rolesSubject = new BehaviorSubject<string[]>([]);
   public roles$ = this.rolesSubject.asObservable();
 
   constructor() { }
-
+  /**
+   * Method which copies the given roles to this class' rolesSubject variable.
+   * @param roles Roles extracted from a user's token.
+   */
   setRoles(roles: string[]) {
     this.rolesSubject.next(roles);
   }
