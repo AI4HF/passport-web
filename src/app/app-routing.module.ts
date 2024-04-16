eb0280d0e5cd0f6c76901e8baa547c257bb1528e
keremyilmaz <keremyilmaz499@gmail.com>
2024-04-16 11:30:12 +0100

    :sparkles: Authguard implemented and applied to the Study Page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { StudyPageComponent} from "./components/study-page/study-page.component";
+import {AuthGuard} from "./service/authguard.service";
 
 /**
  * Routing module which will handle the redirections as new pages are implemented
@@ -9,7 +10,7 @@ import { StudyPageComponent} from "./components/study-page/study-page.component"
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'study_page', component: StudyPageComponent}
+  { path: 'study_page', component: StudyPageComponent, canActivate: [AuthGuard] }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
