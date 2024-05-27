e5efca76872052d8e3ed0cf57ab14fea92eb48ec
okanmercan <okanmercan16@gmail.com>
2024-05-27 11:25:49 +0300

    :sparkles: feat(StudyManagementEdit): Add timeline component. Initialize formGroup.

--- src/app/modules/study-management/study-management-edit/study-management-edit.component.ts
+++ src/app/modules/study-management/study-management-edit/study-management-edit.component.ts
@@ -2,6 +2,7 @@ import {Component, Injector, OnInit} from '@angular/core';
 import {BaseComponent} from "../../../shared/components/base.component";
 import {takeUntil} from "rxjs";
 import {Study} from "../../../shared/models/study.model";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 /**
  * A controller to edit the details of a Study object
@@ -13,14 +14,55 @@ import {Study} from "../../../shared/models/study.model";
 })
 export class StudyManagementEditComponent extends BaseComponent implements OnInit {
 
-  study: Study;
+  /**
+   * The selected study for the component
+   */
+  selectedStudy: Study;
+
+  /**
+   * The steps of a study
+   */
+  studySteps: any[];
+
+  /**
+   * The form object keeping the study information.
+   */
+  studyForm: FormGroup;
+
   constructor(protected injector: Injector) {
     super(injector);
   }
 
   ngOnInit() {
     this.route.data.pipe(takeUntil(this.destroy$)).subscribe(data =>{
-      this.study = data['study'];
+      this.selectedStudy = data['study'];
+      this.initializeForm();
+    });
+
+    this.studySteps = [
+      {name: 'Study Details'},
+      {name: 'Created Populations'},
+      {name: 'Assigned Personnel'},
+      {name: 'Created Experiments'},
+      {name: 'Created Survey Questions'},
+    ];
+  }
+
+  /**
+   * Initializes the form object for the given study.
+   */
+  initializeForm() {
+
+    const name = this.selectedStudy.name ?? '';
+    const description = this.selectedStudy.description ?? '';
+    const objectives = this.selectedStudy.objectives ?? '';
+    const ethics = this.selectedStudy.ethics ?? '';
+
+    this.studyForm = new FormGroup({
+      name: new FormControl(name, Validators.required),
+      description: new FormControl(description, Validators.required),
+      objectives: new FormControl(objectives, Validators.required),
+      ethics: new FormControl(ethics, Validators.required)
     });
   }
 }
--- src/app/modules/study-management/study-management-edit/study-management-edit.module.ts
+++ src/app/modules/study-management/study-management-edit/study-management-edit.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {StudyManagementEditComponent} from "./study-management-edit.component";
+import {CardModule} from "primeng/card";
+import {InputTextModule} from "primeng/inputtext";
+import {TranslateModule} from "@ngx-translate/core";
+import {ReactiveFormsModule} from "@angular/forms";
+import {TimelineModule} from "primeng/timeline";
 
 
 
 @NgModule({
   declarations: [StudyManagementEditComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CardModule,
+    InputTextModule,
+    TranslateModule,
+    ReactiveFormsModule,
+    TimelineModule
   ]
 })
 export class StudyManagementEditModule { }
