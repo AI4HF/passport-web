b9ccd1d52fa0f8092209ac1260b978d06196e323
keremyilmaz <keremyilmaz499@gmail.com>
2024-06-06 11:45:50 +0100

    :sparkles: Organization routes are introduced to the navbar. Personnel page is ensured to not appear in the case of no Organizations being owned.

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -1,20 +1,40 @@
-import {Injector, OnInit} from '@angular/core';
+import { Injector, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { Component } from '@angular/core';
-import {BaseComponent} from "../shared/components/base.component";
+import { BaseComponent } from "../shared/components/base.component";
+import { MenuItem } from 'primeng/api';
 
 @Component({
     selector: 'app-menu',
     templateUrl: './app.menu.component.html'
 })
-export class AppMenuComponent extends BaseComponent implements OnInit {
+export class AppMenuComponent extends BaseComponent implements OnInit, OnDestroy {
 
-    model: any[] = [];
+    model: MenuItem[] = [];
+    private currentOrganizationId: string | null = null;
 
-    constructor(private injector: Injector) {
+    constructor(private injector: Injector, private cdr: ChangeDetectorRef) {
         super(injector);
     }
 
     ngOnInit() {
+        this.currentOrganizationId = localStorage.getItem('currentOrganizationId');
+        this.updateMenuItems();
+
+        window.addEventListener('storage', this.handleStorageChange.bind(this));
+    }
+
+    private handleStorageChange(event: StorageEvent) {
+        if (event.key === 'currentOrganizationId') {
+            this.currentOrganizationId = event.newValue;
+            this.updateMenuItems();
+            this.cdr.detectChanges();
+        }
+    }
+
+    /**
+     * Place to add new connections, now ensuring that if a current organization is not available, its personnel are not shown.
+     */
+    private updateMenuItems() {
         this.model = [
             {
                 label: this.translateService.instant('Manage Studies'),
@@ -24,8 +44,20 @@ export class AppMenuComponent extends BaseComponent implements OnInit {
             {
                 label: this.translateService.instant('Organization'),
                 icon: 'pi pi-home',
-                routerLink: ['/']
+                items: [
+                    {
+                        label: this.translateService.instant('Organization'),
+                        icon: 'pi pi-building',
+                        routerLink: ['/organization-management/organization']
+                    },
+                    ...(this.currentOrganizationId ? [{
+                        label: this.translateService.instant('Personnel'),
+                        icon: 'pi pi-users',
+                        routerLink: ['/organization-management/personnel']
+                    }] : [])
+                ]
             }
         ];
     }
 }
+
