7736c10d1f688345a889f7ef961e437509ec5ef8
keremyilmaz <keremyilmaz499@gmail.com>
2024-06-06 09:25:36 +0100

    :sparkles: Authorization tools implemented as a base for backend-frontend communication.

---
+++ src/app/core/services/auth-management.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {catchError, map, Observable, of} from 'rxjs';
+import {environment} from "../../../environments/environment";
+
+/**
+ * API Service which hosts the available HTTP Requests to the scripts as functions
+ */
+@Injectable({
+    providedIn: 'root',
+})
+
+export class ApiService {
+    /**
+     * @private apiUrl which the request are being sent to
+     */
+    private readonly apiUrl: string;
+
+    /**
+     * apiUrl is set to the url provided in the environment.ts file
+     * @param http
+     */
+    constructor(private http: HttpClient) {
+        this.apiUrl = environment.PASSPORT_API_URL;
+    }
+
+    /**
+     *  AUTHORIZATION API
+     */
+
+    /**
+     * Login request which returns a Token String, which can be used for further authentication and to obtain
+     * @param username Username that will be provided in the form
+     * @param password Password that will be provided in the form
+     */
+    login(username: string, password: string): Observable<any> {
+        const url = `${this.apiUrl}/user/login`;
+        const headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this.http.post(url, {username, password}, {headers, responseType: 'text'});
+    }
+}
