78d33f291c83b4864971ef05e6f9cc839f581b8c
keremyilmaz <keremyilmaz499@gmail.com>
2024-06-21 09:10:17 +0100

    :sparkles: Menu component is updated to add new access routes for new modules.

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -1,30 +1,72 @@
-import {Injector, OnInit} from '@angular/core';
+import { Injector, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { Component } from '@angular/core';
-import {BaseComponent} from "../shared/components/base.component";
+import { BaseComponent } from "../shared/components/base.component";
+import { MenuItem } from 'primeng/api';
 
+/**
+ * Menu component which handles the items on the header
+ */
 @Component({
     selector: 'app-menu',
     templateUrl: './app.menu.component.html'
 })
-export class AppMenuComponent extends BaseComponent implements OnInit {
+export class AppMenuComponent extends BaseComponent implements OnInit, OnDestroy {
 
-    model: any[] = [];
+    model: MenuItem[] = [];
+    private currentOrganizationId: string | null = null;
 
-    constructor(private injector: Injector) {
+    constructor(private injector: Injector, private cdr: ChangeDetectorRef) {
         super(injector);
     }
 
     ngOnInit() {
+        this.currentOrganizationId = localStorage.getItem('organizationId');
+        this.updateMenuItems();
+
+        window.addEventListener('storage', this.handleStorageChange.bind(this));
+    }
+
+    /**
+     * Method which checks if there is a current organization and makes the personnel tab appear or not appear.
+     */
+    private handleStorageChange(event: StorageEvent) {
+        if (event.key === 'organizationId') {
+            this.currentOrganizationId = event.newValue;
+            this.updateMenuItems();
+            this.cdr.detectChanges();
+        }
+    }
+
+    /**
+     * Place to add new connections, now ensuring that if a current organization is not available, its personnel are not shown.
+     */
+    private updateMenuItems() {
         this.model = [
             {
-                label: this.translateService.instant('Study Management'),
+                label: this.translateService.instant('Manage Studies'),
                 icon: 'pi pi-book',
                 routerLink: ['/study-management']
             },
             {
                 label: this.translateService.instant('Organization'),
                 icon: 'pi pi-home',
-                routerLink: ['/']
+                items: [
+                    {
+                        label: this.translateService.instant('Organization'),
+                        icon: 'pi pi-building',
+                        routerLink: ['/organization-management/organization']
+                    },
+                    ...(this.currentOrganizationId ? [{
+                        label: this.translateService.instant('Personnel'),
+                        icon: 'pi pi-users',
+                        routerLink: ['/organization-management/personnel']
+                    }] : [])
+                ]
+            },
+            {
+                label: this.translateService.instant('Survey Management'),
+                icon: 'pi pi-chart-line',
+                routerLink: ['/survey-management']
             }
         ];
     }
