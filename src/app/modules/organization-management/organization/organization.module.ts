abdb7cd1ec9a126741d8d251305b52380e45e85b
keremyilmaz <keremyilmaz499@gmail.com>
2024-06-26 09:35:14 +0100

    :art: Small file refactor.

--- src/app/modules/organization-management/organization/organization.module.ts
+++ src/app/modules/organization-management/organization/organization.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrganizationTableModule } from './organization-table/organization-table.module';
 import { OrganizationFormModule } from './organization-form/organization-form.module';
-import {OrganizationRoutingModule} from "./organization-routing.module";
+import { OrganizationRoutingModule } from './organization-routing.module';
 
 @NgModule({
     imports: [
