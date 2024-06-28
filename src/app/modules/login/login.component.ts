2b85e2a5507f2a5b87df261017da6d795ddee85f
keremyilmaz <keremyilmaz499@gmail.com>
2024-06-28 13:41:39 +0100

    :sparkles: Login component has been edited with the non-mock login service.

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,6 +1,8 @@
 import {Component, Injector, OnInit} from '@angular/core';
 import {BaseComponent} from "../../shared/components/base.component";
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { AuthorizationService } from '../../core/services/authorization.service';
+import {Credentials} from "../../shared/models/credentials.model"; // Update the path as needed
 
 @Component({
     templateUrl: './login.component.html',
@@ -9,7 +11,10 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class LoginComponent extends BaseComponent implements OnInit {
     loginForm: FormGroup;
 
-    constructor(protected injector: Injector) {
+    constructor(
+        protected injector: Injector,
+        private authorizationService: AuthorizationService // Inject the service
+    ) {
         super(injector);
         this.loginForm = new FormGroup({
             username: new FormControl('', Validators.required),
@@ -34,16 +39,18 @@ export class LoginComponent extends BaseComponent implements OnInit {
     }
 
     login() {
-        const {username, password, rememberMe} = this.loginForm.value;
-        // TODO: Implement actual authentication logic here
-        const token = 'mock';
+            const { username, password, rememberMe } = this.loginForm.value;
 
-        if (rememberMe) {
-            localStorage.setItem('token', token);
-        } else {
-            sessionStorage.setItem('token', token);
-        }
-        this.router.navigate(['/study-management']);
+            this.authorizationService.login(new Credentials({username, password})).subscribe(
+                token => {
+                    if (rememberMe) {
+                        localStorage.setItem('token', token);
+                    } else {
+                        sessionStorage.setItem('token', token);
+                    }
+                    this.router.navigate(['/study-management']);
+                }
+            );
     }
 }
 
