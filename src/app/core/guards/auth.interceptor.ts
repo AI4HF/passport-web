246c0ba51240de14ba923651e7af02bc3c922f8f
keremyilmaz <keremyilmaz499@gmail.com>
2024-06-28 13:42:20 +0100

    :art: Auth Interceptor's token serving mechanism is activated.

--- src/app/core/guards/auth.interceptor.ts
+++ src/app/core/guards/auth.interceptor.ts
@@ -13,15 +13,14 @@ export class AuthInterceptor implements HttpInterceptor {
     constructor(private router: Router) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        //TODO: To be activated when the login logic is implemented.
-        /*const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+        const token = localStorage.getItem('token') || sessionStorage.getItem('token');
         if (token) {
             req = req.clone({
                 setHeaders: {
                     Authorization: `Bearer ${token}`
                 }
             });
-        }*/
+        }
 
         return next.handle(req).pipe(
             catchError((error: HttpErrorResponse) => {
