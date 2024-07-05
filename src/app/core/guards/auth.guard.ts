a7245f5b5c40425ac528437f08f4f5ce5976550d
keremyilmaz <keremyilmaz499@gmail.com>
2024-07-05 11:56:00 +0100

    :art: Storage Util method applications are set.

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,17 +1,16 @@
 import { CanActivateFn } from "@angular/router";
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
+import { StorageUtil } from '../services/storageUtil.service';
 
 /**
  * A guard that prevents access to routes if the user is not authenticated.
  */
 export const authGuard: CanActivateFn = () => {
     const router = inject(Router);
+    const storageUtilService = new StorageUtil();
 
-    const localStorageToken = localStorage.getItem('token');
-    const sessionStorageToken = sessionStorage.getItem('token');
-
-    if (!localStorageToken && !sessionStorageToken) {
+    if (storageUtilService.retrieveToken()) {
         router.navigate(['/login']);
         return false;
     }
--- src/app/core/guards/auth.interceptor.ts
+++ src/app/core/guards/auth.interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import {StorageUtil} from "../services/storageUtil.service";
 
 /**
  * An HTTP interceptor which sets up all requests' Authorization headers and logouts if there is an Unauthorized check.
@@ -12,8 +13,12 @@ import { Router } from '@angular/router';
 export class AuthInterceptor implements HttpInterceptor {
     constructor(private router: Router) {}
 
+    /**
+     * Interceptor which controls tasks related to token injection and authorization control on inner pages.
+     */
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+        const storageUtilService = new StorageUtil();
+        const token = storageUtilService.retrieveToken();
         if (token) {
             req = req.clone({
                 setHeaders: {
@@ -21,12 +26,15 @@ export class AuthInterceptor implements HttpInterceptor {
                 }
             });
         }
-
+        /**
+         * Second part of the interceptor which checks for the Unauthorized(401) code from requests to:
+         * Detect expired tokens and revokes access.
+         * Detect cases with direct page access without authorization and revokes access.
+         */
         return next.handle(req).pipe(
             catchError((error: HttpErrorResponse) => {
                 if (error.status === 401) {
-                    localStorage.removeItem('token');
-                    sessionStorage.removeItem('token');
+                    storageUtilService.removeToken();
                     this.router.navigate(['/login']);
                 }
                 return throwError(error);
