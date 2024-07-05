52e8e652e0978cae6f1f85e0c9f58dbd8b1dd861
keremyilmaz <keremyilmaz499@gmail.com>
2024-07-05 11:36:48 +0100

    :memo: Login component documentation and Storage Util applications.

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,9 +1,13 @@
-import {Component, Injector, OnInit} from '@angular/core';
-import {BaseComponent} from "../../shared/components/base.component";
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Component, Injector, OnInit } from '@angular/core';
+import { BaseComponent } from "../../shared/components/base.component";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthorizationService } from '../../core/services/authorization.service';
-import {Credentials} from "../../shared/models/credentials.model"; // Update the path as needed
+import { Credentials } from "../../shared/models/credentials.model";
+import { StorageUtil } from '../../core/services/storageUtil.service';
 
+/**
+ * Login component which handles the main authorization prospects of the system.
+ */
 @Component({
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss']
@@ -11,9 +15,16 @@ import {Credentials} from "../../shared/models/credentials.model"; // Update the
 export class LoginComponent extends BaseComponent implements OnInit {
     loginForm: FormGroup;
 
+    /**
+     * Constructor which sets up the parent component injections and the login form.
+     * @param injector Element injector from Base Component.
+     * @param authorizationService Authorization HTTP requests.
+     * @param storageUtilService Utility service for handling token storage.
+     */
     constructor(
         protected injector: Injector,
-        private authorizationService: AuthorizationService // Inject the service
+        private authorizationService: AuthorizationService,
+        private storageUtilService: StorageUtil
     ) {
         super(injector);
         this.loginForm = new FormGroup({
@@ -23,34 +34,41 @@ export class LoginComponent extends BaseComponent implements OnInit {
         });
     }
 
+    /**
+     * Initial check to skip the login process if the user is already logged in.
+     */
     ngOnInit() {
         this.checkRememberedUser();
     }
 
+    /**
+     * Color scheme adapter.
+     */
     get dark(): boolean {
         return this.layoutService.config().colorScheme !== 'light';
     }
 
+    /**
+     * Remembered user check to skip the login process if necessary.
+     */
     checkRememberedUser() {
-        const token = localStorage.getItem('token')||sessionStorage.getItem('token');
+        const token = this.storageUtilService.retrieveToken();
         if (token) {
             this.router.navigate(['/study-management']);
         }
     }
 
+    /**
+     * Login logic which calls the Authorization Service to handle the credential checks and token storage.
+     */
     login() {
-            const { username, password, rememberMe } = this.loginForm.value;
-
-            this.authorizationService.login(new Credentials({username, password})).subscribe(
-                token => {
-                    if (rememberMe) {
-                        localStorage.setItem('token', token);
-                    } else {
-                        sessionStorage.setItem('token', token);
-                    }
-                    this.router.navigate(['/study-management']);
-                }
-            );
+        const { username, password, rememberMe } = this.loginForm.value;
+        this.authorizationService.login(new Credentials({ username, password })).subscribe(
+            token => {
+                this.storageUtilService.storeToken(token, rememberMe);
+                this.router.navigate(['/study-management']);
+            }
+        );
     }
 }
 
