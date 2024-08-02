482b3225ce04933a92bb7835fb9a5e1a7c32277c
keremyilmaz <keremyilmaz499@gmail.com>
2024-08-02 14:46:42 +0100

    :refactor: Refactor with respect to the new personnel id data type.

--- src/app/core/services/passport.service.ts
+++ src/app/core/services/passport.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {catchError, map, Observable} from "rxjs";
 import {environment} from "../../../environments/environment";
 import {Passport} from "../../shared/models/passport.model";
+import {StorageUtil} from "./storageUtil.service";
 
 /**
  * Service to manage the passport.
@@ -85,6 +86,9 @@ export class PassportService {
      */
     createPassport(passport: Passport): Observable<Passport>{
         const url = `${this.endpoint}`;
+        passport.createdBy = StorageUtil.retrieveUserId();
+        passport.approvedBy = StorageUtil.retrieveUserId();
+
         return this.httpClient.post<Passport>(url, passport)
             .pipe(
                 map((response: any) =>{
