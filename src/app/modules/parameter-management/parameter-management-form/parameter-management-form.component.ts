239e789afe152f6337b177a745f56f14ef473c79
keremyilmaz <keremyilmaz499@gmail.com>
2024-09-25 09:34:45 +0100

    :sparkles: Form autofill tool implementation for parameters with data provided from assets.

--- src/app/modules/parameter-management/parameter-management-form/parameter-management-form.component.ts
+++ src/app/modules/parameter-management/parameter-management-form/parameter-management-form.component.ts
@@ -1,18 +1,20 @@
-import {Component, EventEmitter, Injector, Input, OnInit, Output} from '@angular/core';
-import {BaseComponent} from "../../../shared/components/base.component";
-import {Parameter} from "../../../shared/models/parameter.model";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {takeUntil} from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, Injector, Input, OnInit, Output } from '@angular/core';
+import { BaseComponent } from "../../../shared/components/base.component";
+import { Parameter } from "../../../shared/models/parameter.model";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { takeUntil } from "rxjs/operators";
 
 /**
- * Component for creating or updating parameter.
+ * Component for creating or updating parameters.
  */
 @Component({
   selector: 'app-parameter-management-form',
   templateUrl: './parameter-management-form.component.html',
-  styleUrl: './parameter-management-form.component.scss'
+  styleUrls: ['./parameter-management-form.component.scss']
 })
 export class ParameterManagementFormComponent extends BaseComponent implements OnInit {
+
   /** The ID of the parameter to be edited */
   @Input() parameterId: number;
   /** Event emitted when the form is closed */
@@ -20,15 +22,25 @@ export class ParameterManagementFormComponent extends BaseComponent implements O
 
   /** The selected parameter */
   selectedParameter: Parameter;
+
   /** Form group for parameter form controls */
   parameterForm: FormGroup;
+
   /** flag indicating that dialog is visible */
   display = false;
+
+  /** List of hardcoded parameters loaded from JSON */
+  hardcodedParameters: Parameter[] = [];
+
+  /** Filtered list for auto-fill suggestions */
+  filteredParameters: Parameter[] = [];
+
   /**
    * Constructor to inject dependencies.
    * @param injector The dependency injector
+   * @param http The HTTP client for loading JSON
    */
-  constructor(protected injector: Injector) {
+  constructor(protected injector: Injector, private http: HttpClient) {
     super(injector);
   }
 
@@ -37,10 +49,11 @@ export class ParameterManagementFormComponent extends BaseComponent implements O
    */
   ngOnInit() {
     this.loadParameter();
+    this.loadHardcodedParameters();
   }
 
   /**
-   * Loads the parameter details if a survey ID is provided.
+   * Loads the parameter details if a parameter ID is provided.
    */
   loadParameter() {
     if (this.parameterId) {
@@ -59,9 +72,20 @@ export class ParameterManagementFormComponent extends BaseComponent implements O
             }
           });
     } else {
-      this.selectedParameter = new Parameter({studyId: this.activeStudyService.getActiveStudy().id});
+      this.selectedParameter = new Parameter({});
       this.initializeForm();
     }
+  }
+
+  /**
+   * Loads the hardcoded parameter list from a JSON file.
+   */
+  loadHardcodedParameters() {
+    this.http.get<Parameter[]>('assets/data/hardcoded-parameters.json').pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: data => this.hardcodedParameters = data,
+          error: err => console.error('Failed to load hardcoded parameters', err)
+        });
   }
 
   /**
@@ -77,68 +101,49 @@ export class ParameterManagementFormComponent extends BaseComponent implements O
   }
 
   /**
+   * Filters the hardcoded parameters based on user input.
+   */
+  filterParameters(event: any) {
+    const query = event.query.toLowerCase();
+    this.filteredParameters = this.hardcodedParameters.filter(p => p.name.toLowerCase().includes(query));
+  }
+
+  /**
+   * Auto-fills the form with the selected hardcoded parameter.
+   */
+  selectAutoFill(event: any) {
+    const selectedParameter = event.value;
+    this.parameterForm.setValue({
+      name: selectedParameter.name,
+      dataType: selectedParameter.dataType,
+      description: selectedParameter.description
+    });
+  }
+
+  /**
    * Saves the parameter.
    */
   saveParameter() {
-    if(!this.selectedParameter.parameterId){
-      const newParameter: Parameter = new Parameter({studyId: this.activeStudyService.getActiveStudy().id, ...this.parameterForm.value});
-      this.parameterService.createParameter(newParameter)
-          .pipe(takeUntil(this.destroy$))
-          .subscribe({
-            next: parameter => {
-              this.selectedParameter = parameter;
-              this.initializeForm();
-              this.messageService.add({
-                severity: 'success',
-                summary: this.translateService.instant('Sucess'),
-                detail: this.translateService.instant('ParameterManagement.Parameter is created successfully')
-              });
-            },
-            error: (error: any) => {
-              this.messageService.add({
-                severity: 'error',
-                summary: this.translateService.instant('Error'),
-                detail: error.message
-              });
-            },
-            complete: () => {
-              this.closeDialog();
-            }
+    if (!this.selectedParameter.parameterId) {
+      const newParameter: Parameter = new Parameter({ ...this.parameterForm.value });
+      this.parameterService.createParameter(newParameter).pipe(takeUntil(this.destroy$))
+          .subscribe(() => {
+            this.closeDialog();
           });
-    }else{
-      const updatedParameter: Parameter = new Parameter({studyId: this.activeStudyService.getActiveStudy().id, parameterId: this.selectedParameter.parameterId, ...this.parameterForm.value});
-      this.parameterService.updateParameter(updatedParameter)
-          .pipe(takeUntil(this.destroy$))
-          .subscribe({
-            next: (parameter: Parameter) => {
-              this.selectedParameter = parameter;
-              this.initializeForm();
-              this.messageService.add({
-                severity: 'success',
-                summary: this.translateService.instant('Sucess'),
-                detail: this.translateService.instant('ParameterManagement.Parameter is updated successfully')
-              });
-            },
-            error: (error: any) => {
-              this.messageService.add({
-                severity: 'error',
-                summary: this.translateService.instant('Error'),
-                detail: error.message
-              });
-            },
-            complete: () => {
-              this.closeDialog();
-            }
+    } else {
+      const updatedParameter: Parameter = new Parameter({ parameterId: this.selectedParameter.parameterId, ...this.parameterForm.value });
+      this.parameterService.updateParameter(updatedParameter).pipe(takeUntil(this.destroy$))
+          .subscribe(() => {
+            this.closeDialog();
           });
     }
   }
 
   /**
-   * Closes the dialog
+   * Closes the dialog.
    */
   closeDialog() {
     this.display = false;
     this.formClosed.emit();
   }
-
 }
