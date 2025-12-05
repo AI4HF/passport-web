86ed64d1d4f16dbd1c6c44fe0e736fe9f79c3778
keremyilmaz <keremyilmaz499@gmail.com>
2025-12-05 13:18:46 +0300

    :bug: Issue related to .ts imports is fixed

--- src/app/modules/study-management/study-management-edit/linked-articles/linked-articles-routing.module.ts
+++ src/app/modules/study-management/study-management-edit/linked-articles/linked-articles-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LinkedArticlesTableComponent } from './linked-articles-table/linked-articles-table.component.ts';
+import { LinkedArticlesTableComponent } from './linked-articles-table/linked-articles-table.component';
 
 const routes: Routes = [
     {
